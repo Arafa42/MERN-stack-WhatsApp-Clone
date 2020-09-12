984aa2ac0be9d42021ff23cb8cd5cdad8aa5e687
Arafa Yoncalik <55528241+Arafa42@users.noreply.github.com>
2020-09-13 00:56:52 +0200

    top sidebar completed

--- MERN STACK WHATSAPP CLONE/whatsapp/src/Sidebar.js
+++ MERN STACK WHATSAPP CLONE/whatsapp/src/Sidebar.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {Avatar, IconButton} from "@material-ui/core";
 
 
 
 function sidebar() {
     return (
         <div className="sidebar">            
-<h1>sidebar test</h1>
 
 <div className="sidebar_header">
 
+<Avatar src="https://www.ap.be/sites/default/files/2020-07/alvanjongsafaan.jpg"/>
+
 <div className="sidebar_header_right">
 
+<IconButton>
+<DonutLargeIcon/>
+</IconButton>
 
+<IconButton>
+<ChatIcon/>
+</IconButton>
 
+<IconButton>
+<MoreVertIcon/>
+</IconButton>
 
 
 </div>
 </div>
+
+<div className="sidebar_search">
+
+search
+
+
+</div>
+
 </div>
 
 );
