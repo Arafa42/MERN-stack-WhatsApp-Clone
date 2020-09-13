d050b2ec72ca3bf03d6d325c660a4d8464ca4df7
Arafa Yoncalik <55528241+Arafa42@users.noreply.github.com>
2020-09-13 17:38:45 +0200

    chat top layout finished

--- MERN STACK WHATSAPP CLONE/whatsapp/src/Chat.js
+++ MERN STACK WHATSAPP CLONE/whatsapp/src/Chat.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import "./Chat.css"
+import { Avatar, IconButton } from '@material-ui/core';
+import React from 'react';
+import "./Chat.css";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVert from "@material-ui/icons/MoreVert";
 
 
 
 function Chat() {
     return (
-        <div className="chat">
 
-        <h1>Chat test</h1>
- 
-        </div>
-    );
+<div className="chat">
+<div className="chat_avatar">
+<Avatar/>
+
+<div className="chat_header_info">
+<h3>Dance room</h3>
+<p>last seen at...</p>
+</div>        
+
+<div className="chat_header_icons">
+    <IconButton>
+    <SearchOutlined/>  
+    </IconButton>
+
+    <IconButton>       
+    <AttachFile/>  
+    </IconButton>
+    
+    <IconButton>
+    <MoreVert/>  
+    </IconButton>
+
+</div>   
+</div>
+</div>
+);
 }
 
 export default Chat;
